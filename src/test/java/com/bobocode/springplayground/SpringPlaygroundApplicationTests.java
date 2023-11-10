56fb46d2b680e3e49d34b8ee1ae8b59c1103ddcc
levik <zhenya12@gmail.com>
2023-11-10 18:09:43 +0200

    first commit

---
+++ src/test/java/com/bobocode/springplayground/SpringPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobocode.springplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
